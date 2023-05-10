451877c0c64a8341eaf14af69cc199bc4ea0196a
A Doubt <adoubt2@gmail.com>
2023-05-10 16:40:18 +0200

    Added unit tests

--- repositories/question.test.js
+++ repositories/question.test.js
@@ -40,4 +40,252 @@ describe('question repository', () => {
 
     expect(await questionRepo.getQuestions()).toHaveLength(2)
   })
+
+  test('should return a question with the given id', async () => {
+    const idToFetch = faker.datatype.uuid()
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: idToFetch,
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    const question = await questionRepo.getQuestionById(idToFetch)
+
+    expect
+    expect(question.id).toBe(idToFetch)
+    expect(question).toStrictEqual(testQuestions.find(q => q.id === idToFetch))
+  })
+
+  test('should return undefined when a question is not found', async () => {
+    const nonExistentId = faker.datatype.uuid()
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    const noQuestion = await questionRepo.getQuestionById(nonExistentId)
+    expect(noQuestion).toBeUndefined()
+  })
+
+  test('should add a new question and return it', async () => {
+    const testQuestion = {
+      id: faker.datatype.uuid(),
+      summary: 'How do you make the number one disappear?',
+      author: 'Somebody',
+      answers: [
+        {
+          id: faker.datatype.uuid(),
+          author: 'Me',
+          summary: 'Add a "G" and it\'s gone!'
+        }
+      ]
+    }
+    const returnedQuestion = await questionRepo.addQuestion(testQuestion)
+
+    expect(returnedQuestion).toBeTruthy()
+    expect(returnedQuestion).toStrictEqual(testQuestion)
+  })
+
+  test('should fail to add a new question with duplicate summary', async () => {
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    const returnedQuestion = await questionRepo.addQuestion(testQuestions[0])
+
+    expect(returnedQuestion).toBeFalsy()
+    expect(returnedQuestion).toBeNull()
+  })
+
+  test('should fail to add a new question with malformed body', async () => {
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify([]))
+    const testQuestion = {
+      hello: 'bob',
+      notAnAnswer: 123
+    }
+
+    const returnedQuestion = await questionRepo.addQuestion(testQuestion)
+
+    expect(await questionRepo.getQuestions()).toHaveLength(0)
+    expect(returnedQuestion).toBeFalsy()
+    expect(returnedQuestion).toBeNull()
+  })
+
+  test('should return answers by question id', async () => {
+    const idToFetch = faker.datatype.uuid()
+    const answers = [
+      {
+        id: faker.datatype.uuid(),
+        author: 'Author1',
+        summary: 'answer1'
+      },
+      {
+        id: faker.datatype.uuid(),
+        author: 'Author2',
+        summary: 'answer2'
+      }
+    ]
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: idToFetch,
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: answers
+      }
+    ]
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    const returnedAnswers = await questionRepo.getAnswers(idToFetch)
+
+    expect(returnedAnswers).toHaveLength(2)
+    expect(returnedAnswers).toStrictEqual(answers)
+  })
+
+  test('should return empty array if there are no answers', async () => {
+    const idToFetch = faker.datatype.uuid()
+    const answers = []
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: idToFetch,
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: answers
+      }
+    ]
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    const returnedAnswers = await questionRepo.getAnswers(idToFetch)
+    expect(returnedAnswers).toHaveLength(0)
+    expect(returnedAnswers).toStrictEqual([])
+  })
+
+  test('should return null as answers by non-existent question id', async () => {
+    const nonExistentId = faker.datatype.uuid()
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    const returnedAnswers = await questionRepo.getAnswers(nonExistentId)
+
+    expect(returnedAnswers).toBeNull()
+  })
+
+  test('should add a new answer and return it', async () => {
+    const answer = {
+      id: faker.datatype.uuid(),
+      author: 'Author1',
+      summary: 'answer1'
+    }
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    const questionId = testQuestions[0].id
+    const returnedAnswer = await questionRepo.addAnswer(questionId, answer)
+
+    expect(await questionRepo.getAnswers(questionId)).toHaveLength(1)
+    expect(returnedAnswer).toBeTruthy()
+    expect(returnedAnswer).toMatchObject(answer)
+  })
+
+  test('should fail to add an answer with duplicate summary', async () => {
+    const answer = {
+      id: faker.datatype.uuid(),
+      author: 'Author1',
+      summary: 'answer1'
+    }
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: [answer]
+      },
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    const questionId = testQuestions[0].id
+    const returnedAnswer = await questionRepo.addAnswer(questionId, answer)
+
+    expect(await questionRepo.getAnswers(questionId)).toHaveLength(1)
+    expect(returnedAnswer).toBeNull()
+  })
 })
